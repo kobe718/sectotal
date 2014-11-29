1a8d8dc602d308bcd385d22f722596024a694512
kobe <kobe718@gmail.com>
2014-11-29 18:55:52 +0800

    first commit

---
+++ src/db/monogo/monogo.py
@@ -0,0 +1,17 @@
+'''
+Created on 2014Äê11ÔÂ29ÈÕ
+
+@author: jing
+'''
+
+class monogo(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/web/main.py
@@ -0,0 +1,8 @@
+'''
+Created on 2014Äê11ÔÂ29ÈÕ
+
+@author: jing
+'''
+
+if __name__ == '__main__':
+    pass
